cb7cb7b56f11e516dee85be03e380442fcd89777
BlackTechX <155609122+BlackTechX011@users.noreply.github.com>
2024-01-13 15:45:38 +0530

    Add files via upload

---
+++ src/src.ino
@@ -0,0 +1,26 @@
+#include"main.cpp"
+
+
+void setup() {
+  // set the WiFi chip to "promiscuous" mode aka monitor mode
+
+  Serial.begin(115200);
+  delay(10);
+  wifi_set_opmode(STATION_MODE);
+  wifi_set_channel(1);
+  wifi_promiscuous_enable(DISABLE);
+  delay(10);
+  wifi_set_promiscuous_rx_cb(sniffer_callback);
+  delay(10);
+  wifi_promiscuous_enable(ENABLE);
+
+  // setup the channel hoping callback timer
+  os_timer_disarm(&channelHop_timer);
+  os_timer_setfn(&channelHop_timer, (os_timer_func_t *) channelHop, NULL);
+  os_timer_arm(&channelHop_timer, CHANNEL_HOP_INTERVAL_MS, 1);
+}
+
+void loop() {
+  delay(10);
+  
+}
